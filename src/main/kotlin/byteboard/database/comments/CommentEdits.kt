85b5071b77ae90354888b8f1f89ab5838e29eb1e
Dustyn <dustyn_gibb@hotmail.com>
2024-04-01 17:33:19 -0400

    minor additions to the edit dao files

--- src/main/kotlin/byteboard/database/comments/CommentEdits.kt
+++ src/main/kotlin/byteboard/database/comments/CommentEdits.kt
@@ -5,14 +5,21 @@ import byteboard.database.Users
 import byteboard.database.Users.references
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.javatime.datetime
+import java.time.LocalDateTime
 
-object CommentEdits: Table(name = "CommentEdits") {
+object CommentEdits : Table(name = "CommentEdits") {
     val id: Column<Long> = long("id").autoIncrement()
     val commentId: Column<Long> = long("commentId").references(Comments.id)
     val posterId: Column<Long> = long("posterId").references(Users.id)
-
-
+    val lastEdited : Column<LocalDateTime> = datetime("lastEdited")
 
 
     override val primaryKey = PrimaryKey(id)
 }
+
+data class CommentEdit(
+    val commentId : Long,
+    val posterId : Long,
+    val lastEdited : LocalDateTime
+)
