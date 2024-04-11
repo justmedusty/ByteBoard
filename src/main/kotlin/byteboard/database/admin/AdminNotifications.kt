b7e7873c8fc247defd73a53be23d248d7812213e
Dustyn <dustyn_gibb@hotmail.com>
2024-04-11 06:20:30 -0400

    Not doing 2 suspend logs will just mash it into one

--- src/main/kotlin/byteboard/database/admin/AdminNotifications.kt
+++ src/main/kotlin/byteboard/database/admin/AdminNotifications.kt
@@ -1,14 +1,11 @@
 
 import byteboard.database.admin.SuspendLog
-import byteboard.database.admin.UnsuspendLog
 import org.jetbrains.exposed.sql.Column
-import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
 object AdminNotifications : Table(name = "AdminNotifications") {
     val id: Column<Long> = long("id").autoIncrement()
     val read: Column<Boolean> = bool("read").default(false)
     val suspendId: Column<Long?> = long("suspendId").references(SuspendLog.id).nullable()
-    val unsuspendId: Column<Long?> = long("unsuspendId").references(UnsuspendLog.id).nullable()
 
     override val primaryKey = PrimaryKey(id)
 }
--- src/main/kotlin/byteboard/database/admin/SuspendLog.kt
+++ src/main/kotlin/byteboard/database/admin/SuspendLog.kt
@@ -1,19 +1,50 @@
 package byteboard.database.admin
 
 import byteboard.database.useraccount.Users
+import byteboard.database.useraccount.logger
 import org.jetbrains.exposed.sql.Column
-import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.javatime.datetime
+import org.jetbrains.exposed.sql.transactions.transaction
 import java.time.LocalDateTime
 
 object SuspendLog : Table(name = "SuspendLog") {
     val id: Column<Long> = long("id").autoIncrement()
-    val suspendTimeStamp: Column<LocalDateTime> = datetime("suspendTime")
+    val timestamp: Column<LocalDateTime> = datetime("suspendTime")
     val adminId: Column<Long> = long("message_id").references(Users.id)
-    val reason : Column<String> = text("reason")
+    //true for suspend, false for unsuspend
+    val suspend: Column<Boolean> = bool("suspend")
+    val reason: Column<String> = text("reason")
 
     override val primaryKey = PrimaryKey(id)
 }
 
+
+data class SuspendLogEntry(
+    val id : Long,
+    val time: LocalDateTime,
+    val adminId : Long,
+    val suspend: Boolean,
+    val reason : String
+)
+
+
+fun insertSuspendEntry(userId: Long,isSuspend : Boolean,reasonString: String) : Boolean{
+    return try {
+        transaction {
+            SuspendLog.insert {
+                it[timestamp] = LocalDateTime.now()
+                it[adminId] = userId
+                it[suspend] = isSuspend
+                it[reason] = reasonString
+            }.insertedCount > 0
+
+        }
+    }catch (e:Exception){
+        logger.error { e.message }
+        false
+
+    }
+}
 
